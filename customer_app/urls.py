7c3e0b9dd122d95f819952dcad0117dd4bffc646
Mahbubur Rahman <104581346+mahbubu1640@users.noreply.github.com>
2024-03-19 03:34:19 +0530

    home page updated

--- customer_app/urls.py
+++ customer_app/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 from customer_app import views
 urlpatterns = [
     path('', views.home_view,name='home'),
+    path('login/', views.login_user,name='login'),
+    path('logout/', views.logout_user,name='logout'),
 ]
--- customer_app/views.py
+++ customer_app/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate,login,logout
 # Create your views here.
+from django.contrib import messages
 
 
 def home_view(request):
-    return render(request,'home.html')
+    return render(request,'homepage.html')
+
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    pass
